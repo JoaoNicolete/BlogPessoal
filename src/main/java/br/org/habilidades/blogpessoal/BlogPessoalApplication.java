5fede03c04fc73667c9d527272ce719f8f0966ab
João Nicolete <joao.nicolete@usp.br>
2021-06-11 13:04:03 -0300

    e la vamos nós

--- src/main/java/br/org/habilidades/blogpessoal/BlogPessoalApplication.java
+++ src/main/java/br/org/habilidades/blogpessoal/BlogPessoalApplication.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-
+@SpringBootApplication
 @RestController
 @RequestMapping("/")
-@SpringBootApplication
 public class BlogPessoalApplication {
 
 	 @GetMapping
