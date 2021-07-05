05fca36882cdc1f01bd7d4766f646c4f8828ac63
joão <joao.nicolete@usp.br>
2021-07-05 11:18:30 -0300

    implementando put e delete no tema

--- Font-end/blogPessoal/src/app/service/tema.service.ts
+++ Font-end/blogPessoal/src/app/service/tema.service.ts
@@ -17,12 +17,29 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization', environment.token) 
   }
 
+  refreshToken(){
+    this.token= {
+      headers: new HttpHeaders().set('Authorization', environment.token) 
+    }
+  }
+
   getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>('https://blognicolete.herokuapp.com/tema', this.token)
   }
 
-  postTema(tema: Tema): Observable<Tema>{
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blognicolete.herokuapp.com/tema/${id}`, this.token)
+  }
 
+  postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blognicolete.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blognicolete.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blognicolete.herokuapp.com/tema/${id}`, this.token)
   }
 }
